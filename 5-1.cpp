0b019f7b27041078158b092c879acb854bb0fbc5
EnnoyGK <44551441+EnnoyGK@users.noreply.github.com>
2019-05-24 09:40:59 +0300

    Add files via upload

--- 5-1.cpp
+++ 5-1.cpp
@@ -24,15 +24,15 @@ int main() {
 	tmp t;
 	unsigned int w, h;//Надеемся, что тут 4 байта
 
-	r = 20;
-	g = 0;
-	b = 50;
+	r = 132;
+	g = 150;
+	b = 255;
 
-	rr = 100;
-	gg = 100;
-	bb = 100;
+	rr = 239;
+	gg = 57;
+	bb = 220;
 
-	steps = 500;
+	steps = 10;
 
 
 	fff.read((char *)&buf, 18);   //Чтение 18 байт заголовка bmp
@@ -40,39 +40,68 @@ int main() {
 	fff.read((char *)&w, 4); cout << "w=" << w;   //Чтение width из заголовка bmp
 	fff.read((char *)&w, 4); cout << ", h=" << w; //Чтение height из заголовка bmp
 
-	w = 1024;
+	w = 600;
 	ggg.write((char *)&w, 4);    //Запись width в заголовок bmp ( w кратно 4, обязательно для нас, чтобы не делать выравнивание)
 	
-	h = 512;
+	h = 200;
 	ggg.write((char *)&h, 4);    //Запись height в заголовок bmp
 	fff.read((char *)&buf, 28);   //Чтение 28 байт заголовка bmp
 	ggg.write((char *)&buf, 28);    //Запись 28 байт заголовка bmp
 
+	if (steps > w)
+		steps = w;
+
+	float miniSteps = w / steps;
+	float tmpSteps = miniSteps;
 	stepr = (float)(rr - r) / steps;
 	stepg = (float)(gg - g) / steps;
 	stepb = (float)(bb - b) / steps;
 	t.r = (float)r;
 	t.g = (float)g;
 	t.b = (float)b; 
-	int l = 0;
 		for (unsigned int i = 1; i <= h; i++){
-			for (unsigned int j = 1; j <= w; j++) {
-				if (l < steps) {
+			for (unsigned int j = 1; j <= w; j++ ) {
+				
+				if (j == miniSteps) {
+					miniSteps += tmpSteps;
 					t.r += stepr;
 					t.g += stepg;
 					t.b += stepb;
-					c.r = (unsigned char)t.r;
-					c.g = (unsigned char)t.g;
-					c.b = (unsigned char)t.g;
-					++l;
 				}
-				
+				c.r = (unsigned char)t.r;
+				c.g = (unsigned char)t.g;
+				c.b = (unsigned char)t.g;
 				ggg.write((char *)&c, 3);
+				
+				
+			
 				
 			}
 			t.r = (float)r;
 			t.g = (float)g;
 			t.b = (float)b;
+		miniSteps = tmpSteps;
+	}
+
+	int l = 0;
+	for (unsigned int i = 1; i <= h; i++) {
+		for (unsigned int j = 1; j <= w; j++) {
+			if (l < steps) {
+				t.r += stepr;
+				t.g += stepg;
+				t.b += stepb;
+				c.r = (unsigned char)t.r;
+				c.g = (unsigned char)t.g;
+				c.b = (unsigned char)t.g;
+				++l;
+			}
+
+			ggg.write((char *)&c, 3);
+
+		}
+		t.r = (float)r;
+		t.g = (float)g;
+		t.b = (float)b;
 		l = 0;
 	}
 	
